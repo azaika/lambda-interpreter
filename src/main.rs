90aa115122b415c776276db782a93b7dd9589117
azaika <takaaza@gmail.com>
2020-07-23 13:29:59 +0900

    import clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("lambda-interpreter")
+        .author("Takashi Nakayama <takashi-nakayama@g.ecc.u-tokyo.ac.jp>")
+        .about("untyped lambda interpreter")
+        .arg(Arg::with_name("LAMBDA")
+            .help("set lambda to interpret like '\\x. x'")
+            .required(true)
+            .index(1)
+        );
+    
+    let match_result = app.get_matches();
+
+    if let Some(lambda) = match_result.value_of("LAMBDA") {
+        println!("input recognized! : {}", lambda);
+    }
 }
